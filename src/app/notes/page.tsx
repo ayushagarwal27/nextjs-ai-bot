1b56db041e3a7f08d4e0d0c51a663b6c53391379
Ayush <ayushagarwal.dev@gmail.com>
2023-12-04 20:56:57 +0530

    Setup: clerk signin and signup

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Flowbrain - Notes",
+};
+
+const NotesPage = () => {
+  return <div>NotesPage</div>;
+};
+
+export default NotesPage;
